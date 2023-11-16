668cff74338f58c25f5480436117d58e61341e7f
Song Liu <song.liu@bristol.ac.uk>
2023-11-16 03:33:40 +0000

    edited code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM nvidia/cuda:12.2.2-devel-ubuntu22.04
+# FROM nvidia/cuda:12.2.2-devel-ubuntu22.04
+FROM ubuntu:latest
 
 ENV TZ=Europe/London
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
@@ -15,14 +16,14 @@ RUN apt-get update && \
 
 RUN apt-get install -y libopenblas-dev cmake unzip libboost-dev
 
-# Install CUDA toolkit
-RUN echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc && \
-    echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+# # Install CUDA toolkit
+# RUN echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc && \
+#     echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 
 RUN nvcc --version
 RUN git clone https://github.com/anewgithubname/Juzhen.git
 WORKDIR /Juzhen
-RUN unzip dataset.zip
+RUN unzip datasets/MNIST/dataset.zip -d datasets/MNIST/
 RUN mkdir build 
 WORKDIR /Juzhen/build
 RUN cmake .. -DCMAKE_CXX_COMPILER=clang++-15
--- ml/dataloader.hpp
+++ ml/dataloader.hpp
@@ -2,7 +2,61 @@
 // Created by songa on 16/11/2023.
 //
 
-#ifndef ANIMATED_OCTO_SNIFFLE_DATALOADER_H
-#define ANIMATED_OCTO_SNIFFLE_DATALOADER_H
+#ifndef ANIMATED_OCTO_SNIFFLE_DATALOADER_HPP
+#define ANIMATED_OCTO_SNIFFLE_DATALOADER_HPP
 
-#endif //ANIMATED_OCTO_SNIFFLE_DATALOADER_H
+#include "../cpp/juzhen.hpp"
+
+
+namespace Juzhen
+{
+    template <class D>
+    class DataLoader {
+        std::string folder;
+        std::string split;
+
+        size_t batch_size;
+        size_t n; 
+        size_t d;
+
+        FILE *fp_input;
+        FILE *fp_output;
+
+    public:
+        DataLoader(std::string folder, std::string split, size_t batch_size): folder(folder), split(split), batch_size(batch_size) {
+            std::string input_file = folder + "/" + split + "_x.txt";
+            std::string output_file = folder + "/" + split + "_y.txt";
+            fp_input = fopen(input_file.c_str(), "r");
+            fp_output = fopen(output_file.c_str(), "r");
+
+            if (fp_input == NULL || fp_output == NULL) {
+                std::cout << "Error opening file" << std::endl;
+                exit(1);
+            }
+
+            d = getw(fp_input);
+            n = getw(fp_input);
+        }
+
+        ~DataLoader() {
+            fclose(fp_input);
+            fclose(fp_output);
+        }
+
+        std::tuple<Matrix<D>, Matrix<D>> next_batch() {
+            Matrix<D> x(batch_size, d);
+            Matrix<D> y(batch_size, 1);
+
+            // determine how many bytes to read given the batch size and the remaining data
+            size_t bytes_to_read = sizeof(D) * d * batch_size;
+            // check how many 
+
+            // fread(x.data(), sizeof(D), d * batch_size, fp_input);
+            // fread(y.data(), sizeof(D), batch_size, fp_output);
+
+            // return {x, y};
+        }
+
+    };
+}
+#endif //ANIMATED_OCTO_SNIFFLE_DATALOADER_HPP
